9a909bce7682285f57c18eb1b240ab33bfd43b9d
Lexinon <willi4wuff@gmail.com>
2025-08-07 19:07:20 +0200

    Added template file

---
+++ Template.lean
@@ -0,0 +1,45 @@
+/-
+Copyright (c) Lexinon. All rights reserved.
+Licensed under the MIT license. See LICENSE file in the project root for details.
+-/
+
+/-!
+# Competition Problem
+
+Problem
+
+Solution
+-/
+
+namespace CompetitionProblem
+
+section Problem
+
+/-! ## Definitions for setting up the problem -/
+
+end Problem
+
+-- ==============================
+
+section Proof
+
+/-! ## Lemmas and definitions for the proof -/
+
+end Proof
+
+-- ==============================
+
+section Result
+
+/-! ## The solution and the proof of its correctness -/
+
+/-- The solution to the problem. -/
+def solution : Nat := sorry
+
+/-- Proof that the above `solution` is correct. -/
+theorem proof : True := by
+  sorry
+
+end Result
+
+end CompetitionProblem
