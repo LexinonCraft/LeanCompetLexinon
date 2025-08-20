ece2defe017011aa7f35784b87b1039ac338c4bc
Lexinon <willi4wuff@gmail.com>
2025-08-20 15:13:27 +0200

    BwM25R1A2 Added comments

--- LeanCompetLexinon/BwM25/R1A2.lean
+++ LeanCompetLexinon/BwM25/R1A2.lean
@@ -26,7 +26,6 @@ and each of them infinitely many times.
 
 ## TODO
 * Second part of the problem
-* Add comments
 * Split up longer tactic proofs
 -/
 
@@ -48,15 +47,19 @@ section Proof
 
 /-! ## Lemmas and definitions for the proof -/
 
+/-- The greatest `k : ℕ` such that `2 ^ k ≤ n`. -/
 def maxTwoPowLe (n : ℕ) : ℕ := Nat.findGreatest (2 ^ · ≤ n) n
 
-@[simp]
 lemma zero_lt_maxTwoPowLe {n : ℕ} : 0 < maxTwoPowLe (n + 2) := by
   unfold maxTwoPowLe
   rw [Nat.findGreatest_pos]
   use 1
   simp
 
+/-! ### Simplified factorization of natural numbers -/
+
+/-- Describes a positive `n : ℕ` with (unique) `m₂ m₅ r : ℕ` such that `n = 2 ^ m₂ * 5 ^ m₅ * r` and
+neither `2` nor `5` divides `r`. -/
 @[ext]
 structure SimpleFactorization : Type where
   m₂ : ℕ
@@ -65,6 +68,7 @@ structure SimpleFactorization : Type where
   hr : ¬2 ∣ r ∧ ¬5 ∣ r
 deriving Repr
 
+/-- Assigns `m` its `SimpleFactorization` (`0` is assigned a garbage value). -/
 def destruct (m : ℕ) : SimpleFactorization where
   m₂ := m.factorization 2
   m₅ := m.factorization 5
@@ -101,7 +105,8 @@ lemma destruct_r_ne_zero {x : SimpleFactorization} : x.r ≠ 0 := by
   rw [ha]
   simp
 
-def restruct (x : SimpleFactorization) : ℕ := 5 ^ x.m₅ * 2 ^ x.m₂ * x.r
+/-- Reconstructs `m : ℕ` from its `SimpleFactorization` (`destruct m`). -/
+def restruct (x : SimpleFactorization) : ℕ := 2 ^ x.m₂ * 5 ^ x.m₅ * x.r
 
 @[simp]
 lemma destruct_restruct {x : SimpleFactorization} : destruct (restruct x) = x := by
@@ -127,15 +132,19 @@ lemma destruct_restruct {x : SimpleFactorization} : destruct (restruct x) = x :=
       lhs
       left
       left
+      rw [mul_assoc, mul_comm]
       left
       rw [mul_comm]
-    rw [mul_assoc]
     simp
 
 @[simp]
 lemma restruct_destruct {m : ℕ} : restruct (destruct (m + 1)) = m + 1 := by
   unfold destruct restruct
   simp
+  conv =>
+    lhs
+    left
+    rw [mul_comm]
   rw [mul_assoc, mul_comm, mul_assoc]
   rw [Nat.div_mul_cancel ?_]
   · rw [mul_comm]
@@ -148,29 +157,10 @@ lemma restruct_destruct {m : ℕ} : restruct (destruct (m + 1)) = m + 1 := by
     · apply Nat.ordProj_dvd
     · apply Nat.ordProj_dvd
 
-lemma factorization_factorial_eq_sum {n p : ℕ} : Nat.factorization (n + 2)! p =
-    ∑ m ∈ Finset.range (n + 2), Nat.factorization (m + 1) p := by
-  rw [← Finset.prod_range_add_one_eq_factorial]
-  rw [← Finset.sum_apply, ← Finsupp.coe_finset_sum, ← Nat.factorization_prod]
-  simp
+/-! ### Elimination partners and pairs -/
 
-lemma sum_factorization_erase_two_pow {n p : ℕ} : let k := maxTwoPowLe (n + 2)
-    ∑ m ∈ Finset.range (n + 2), Nat.factorization (m + 1) p =
-    ∑ m ∈ (Finset.range (n + 2)).erase (2 ^ k - 1), Nat.factorization (m + 1) p +
-    Nat.factorization (2 ^ k) p := by
-  intro k
-  have h : 2 ^ k - 1 ∈ Finset.range (n + 2) := by
-    rw [Finset.mem_range]
-    apply Nat.sub_one_lt_of_le (by simp)
-    exact @Nat.findGreatest_spec 0 (2 ^ · ≤ n + 2) _ _ (by simp) (by simp)
-  rw [eq_comm]
-  rw (config := { occs := .pos [2] }) [← Nat.sub_one_add_one (show 2 ^ k ≠ 0 by simp)]
-  apply Finset.sum_erase_add
-  exact h
-
-def setOfM₅LeM₂SubOne (n : ℕ) : Finset ℕ :=
-  {m ∈ Finset.range n | (destruct (m + 1)).m₅ ≤ (destruct (m + 1)).m₂ ∧ m ≠ 2 ^ maxTwoPowLe n - 1}
-
+/-- For a positive `m : ℕ`, `elimPartner m` is the number you get when you replace each appearence of the
+prime factor `2` in the factorization of `m` with `5` and vice-versa. -/
 def elimPartner (m : ℕ) : ℕ := let ⟨m₂, m₅, r, hr⟩ := destruct m; restruct ⟨m₅, m₂, r, hr⟩
 
 @[simp]
@@ -224,8 +214,8 @@ lemma eq_elimPartner_self {m : ℕ} : (destruct (m + 1)).m₅ = (destruct (m + 1
     rw (config := { occs := .pos [1] }) [← h]
     simp
 
-def elimPairSubOne (n m : ℕ) : Finset ℕ := {m, elimPartner (m + 1) - 1} ∩ Finset.range n
-
+/-- If the factorization of `m + 1` contains the prime factor `2` at least as often as `5` and `m < n`,
+then also `elimPartner (m + 1) - 1 < n`. -/
 lemma elimPartner_mem_range {n m : ℕ} (h₁ : (destruct (m + 1)).m₂ < (destruct (m + 1)).m₅)
     (h₂ : m ∈ Finset.range n) : elimPartner (m + 1) - 1 ∈ Finset.range n := by
   rw [Finset.mem_range] at h₂ ⊢
@@ -241,10 +231,10 @@ lemma elimPartner_mem_range {n m : ℕ} (h₁ : (destruct (m + 1)).m₂ < (destr
   · rw [← Nat.sub_add_cancel h₁.le]
     conv =>
       lhs
-      rw [pow_add, mul_comm, mul_assoc, ← mul_pow]
+      rw [pow_add, mul_assoc, ← mul_pow]
     conv =>
       rhs
-      rw [pow_add, mul_assoc, ← mul_pow]
+      rw [pow_add, ← mul_assoc, mul_comm, ← mul_assoc, ← mul_pow, mul_comm]
     apply Nat.mul_lt_mul_of_pos_right
     · rw [Nat.pow_lt_pow_iff_left (Nat.sub_ne_zero_of_lt h₁)]
       simp
@@ -252,6 +242,7 @@ lemma elimPartner_mem_range {n m : ℕ} (h₁ : (destruct (m + 1)).m₂ < (destr
   · apply Nat.zero_lt_of_ne_zero
     simp
 
+/-- If `m < n + 2`, then `elimPartner (m + 1) - 1 ≠ 2 ^ maxTwoPowLe (n + 2) - 1`. -/
 lemma elimPartner_ne_two_pow_of_mem_range {n m : ℕ} (h : m ∈ Finset.range (n + 2)) :
     elimPartner (m + 1) - 1 ≠ 2 ^ maxTwoPowLe (n + 2) - 1 := by
   by_contra ha
@@ -270,7 +261,7 @@ lemma elimPartner_ne_two_pow_of_mem_range {n m : ℕ} (h : m ∈ Finset.range (n
     rw [pow_mul]
     calc
       _ ≤ 5 ^ maxTwoPowLe (n + 2) := by
-        rw [Nat.pow_le_pow_iff_left (by rw [Nat.ne_zero_iff_zero_lt]; simp)]; simp
+        rw [Nat.pow_le_pow_iff_left (by rw [Nat.ne_zero_iff_zero_lt]; apply zero_lt_maxTwoPowLe)]; simp
       _ = m + 1 := ha.symm
       _ ≤ n + 2 := by rw [Nat.add_one_le_iff, ← Finset.mem_range]; exact h
   absurd show ¬2 ^ (2 * maxTwoPowLe (n + 2)) ≤ n + 2 by
@@ -284,6 +275,78 @@ lemma elimPartner_ne_two_pow_of_mem_range {n m : ℕ} (h : m ∈ Finset.range (n
        _ ≤ n + 2 := h'
   exact h'
 
+/-- The set of `m : ℕ` with `m < n` such that in `m + 1`, the prime factor `2` appears at least as often
+as the prime factor `5` but `m ≠ maxTwoPowLe n - 1`. -/
+def setOfM₅LeM₂SubOne (n : ℕ) : Finset ℕ :=
+  {m ∈ Finset.range n | (destruct (m + 1)).m₅ ≤ (destruct (m + 1)).m₂ ∧ m ≠ 2 ^ maxTwoPowLe n - 1}
+
+/-- For `m ∈ setOfM₅LeM₂SubOne n`, `elimPairSubOne n m` will contains `m` and - if
+`elimPartner (m + 1) ≤ n` - also `elimPartner (m + 1) - 1`. -/
+def elimPairSubOne (n m : ℕ) : Finset ℕ := {m, elimPartner (m + 1) - 1} ∩ Finset.range n
+
+lemma elimPair_eq_singleton_or_pair {n m : ℕ} (hm : m ∈ setOfM₅LeM₂SubOne n) : elimPairSubOne n m = {m} ∨
+    (elimPairSubOne n m = {m, elimPartner (m + 1) - 1} ∧ m ≠ elimPartner (m + 1) - 1) := by
+  unfold setOfM₅LeM₂SubOne at hm
+  rw [Finset.mem_filter] at hm
+  by_cases h : (destruct (m + 1)).m₅ = (destruct (m + 1)).m₂
+  · apply Or.inl
+    apply Finset.Subset.antisymm
+    · calc
+        _ ⊆ {m, elimPartner (m + 1) - 1} := Finset.inter_subset_left
+        _ = {m} := by rw [eq_elimPartner_self.mp h]; simp
+    · unfold elimPairSubOne
+      simp [-Finset.mem_range]
+      exact hm.left
+  · by_cases h' : elimPartner (m + 1) - 1 ∈ Finset.range n
+    · apply Or.inr
+      constructor
+      · apply Finset.Subset.antisymm
+        · exact Finset.inter_subset_left
+        · apply Finset.subset_inter Finset.Subset.rfl
+          rw [Finset.insert_eq, Finset.union_subset_iff]
+          rw [Finset.singleton_subset_iff, Finset.singleton_subset_iff]
+          exact ⟨hm.left, h'⟩
+      · by_contra ha
+        conv at ha =>
+          rw [eq_comm, ← Nat.add_one_inj]
+          simp
+          rw [← eq_elimPartner_self]
+        absurd ha
+        exact h
+    · apply Or.inl
+      apply Finset.Subset.antisymm
+      · unfold elimPairSubOne
+        rw [Finset.insert_eq, Finset.union_inter_distrib_right, Finset.singleton_inter_of_notMem h']
+        rw [Finset.union_empty]
+        exact Finset.inter_subset_left
+      · unfold elimPairSubOne
+        simp [-Finset.mem_range]
+        exact hm.left
+
+/-! ### Factorization of factorials -/
+
+lemma factorization_factorial_eq_sum {n p : ℕ} : Nat.factorization (n + 2)! p =
+    ∑ m ∈ Finset.range (n + 2), Nat.factorization (m + 1) p := by
+  rw [← Finset.prod_range_add_one_eq_factorial]
+  rw [← Finset.sum_apply, ← Finsupp.coe_finset_sum, ← Nat.factorization_prod]
+  simp
+
+lemma sum_factorization_erase_two_pow {n p : ℕ} : let k := maxTwoPowLe (n + 2)
+    ∑ m ∈ Finset.range (n + 2), Nat.factorization (m + 1) p =
+    ∑ m ∈ (Finset.range (n + 2)).erase (2 ^ k - 1), Nat.factorization (m + 1) p +
+    Nat.factorization (2 ^ k) p := by
+  intro k
+  have h : 2 ^ k - 1 ∈ Finset.range (n + 2) := by
+    rw [Finset.mem_range]
+    apply Nat.sub_one_lt_of_le (by simp)
+    exact @Nat.findGreatest_spec 0 (2 ^ · ≤ n + 2) _ _ (by simp) (by simp)
+  rw [eq_comm]
+  rw (config := { occs := .pos [2] }) [← Nat.sub_one_add_one (show 2 ^ k ≠ 0 by simp)]
+  apply Finset.sum_erase_add
+  exact h
+
+/-- The set of `m < n + 2` without `2 ^ maxTwoPowLe (n + 2) - 1` can be described as the union of
+`elimPairSubOne (n + 2) m` for all `m ∈ setOfM₅LeM₂SubOne (n + 1)`. -/
 lemma range_erase_eq_biUnion {n : ℕ} : (Finset.range (n + 2)).erase (2 ^ maxTwoPowLe (n + 2) - 1) =
     Finset.biUnion (setOfM₅LeM₂SubOne (n + 2)) (elimPairSubOne (n + 2)) := by
   apply Finset.ext
@@ -351,6 +414,7 @@ lemma ne_elimPartner_of_mem_of_ne {n x y : ℕ} (hx : x ∈ setOfM₅LeM₂SubOn
     · rw [ne_comm]
       exact h
 
+/-- The sets `elimPairSubOne n m` are pairwise disjoint. -/
 lemma pairwiseDisjoint_elimPair {n : ℕ} : (setOfM₅LeM₂SubOne n : Set ℕ).PairwiseDisjoint
     (elimPairSubOne n) := by
   intros x hx y hy hxy
@@ -382,45 +446,6 @@ lemma pairwiseDisjoint_elimPair {n : ℕ} : (setOfM₅LeM₂SubOne n : Set ℕ).
       absurd ha
       exact hxy
 
-lemma elimPair_eq_singleton_or_pair {n m : ℕ} (hm : m ∈ setOfM₅LeM₂SubOne n) : elimPairSubOne n m = {m} ∨
-    (elimPairSubOne n m = {m, elimPartner (m + 1) - 1} ∧ m ≠ elimPartner (m + 1) - 1) := by
-  unfold setOfM₅LeM₂SubOne at hm
-  rw [Finset.mem_filter] at hm
-  by_cases h : (destruct (m + 1)).m₅ = (destruct (m + 1)).m₂
-  · apply Or.inl
-    apply Finset.Subset.antisymm
-    · calc
-        _ ⊆ {m, elimPartner (m + 1) - 1} := Finset.inter_subset_left
-        _ = {m} := by rw [eq_elimPartner_self.mp h]; simp
-    · unfold elimPairSubOne
-      simp [-Finset.mem_range]
-      exact hm.left
-  · by_cases h' : elimPartner (m + 1) - 1 ∈ Finset.range n
-    · apply Or.inr
-      constructor
-      · apply Finset.Subset.antisymm
-        · exact Finset.inter_subset_left
-        · apply Finset.subset_inter Finset.Subset.rfl
-          rw [Finset.insert_eq, Finset.union_subset_iff]
-          rw [Finset.singleton_subset_iff, Finset.singleton_subset_iff]
-          exact ⟨hm.left, h'⟩
-      · by_contra ha
-        conv at ha =>
-          rw [eq_comm, ← Nat.add_one_inj]
-          simp
-          rw [← eq_elimPartner_self]
-        absurd ha
-        exact h
-    · apply Or.inl
-      apply Finset.Subset.antisymm
-      · unfold elimPairSubOne
-        rw [Finset.insert_eq, Finset.union_inter_distrib_right, Finset.singleton_inter_of_notMem h']
-        rw [Finset.union_empty]
-        exact Finset.inter_subset_left
-      · unfold elimPairSubOne
-        simp [-Finset.mem_range]
-        exact hm.left
-
 lemma factorization_factorial_five_lt_two {n : ℕ} : Nat.factorization (n + 2)! 5 <
     Nat.factorization (n + 2)! 2 := by
   conv =>
@@ -451,63 +476,65 @@ lemma factorization_factorial_five_lt_two {n : ℕ} : Nat.factorization (n + 2)!
     use 1
     simp
 
+/-- If `l ≠ 0` and the factorization of `l` contains the prime factor `2` more often than `5`, then the
+last non-zero digit of the decimal representation of `l` is even. -/
 lemma even_last_non_zero_digit_of_factorization_five_lt_two {l : ℕ} (hl : l ≠ 0)
     (hm₅m₂ : l.factorization 5 < l.factorization 2) :
     ((Nat.digits 10 l).find? (· ≠ 0)).get! % 2 = 0 := by
-  let rec aux (m₅ : ℕ) {l : ℕ} (hl : l ≠ 0) (hm₅m₂ : l.factorization 5 < l.factorization 2) (hm₅ : l.factorization 5 = m₅) : ((Nat.digits 10 l).find? (· ≠ 0)).get! % 2 = 0 := by
-    have bla' : l.factorization 2 ≠ 0 := Nat.ne_zero_of_lt hm₅m₂
-    conv at bla' =>
+  let rec aux (m₅ : ℕ) {l : ℕ} (hl : l ≠ 0) (hm₅m₂ : l.factorization 5 < l.factorization 2)
+      (hm₅ : l.factorization 5 = m₅) : ((Nat.digits 10 l).find? (· ≠ 0)).get! % 2 = 0 := by
+    have hm₂ : l.factorization 2 ≠ 0 := Nat.ne_zero_of_lt hm₅m₂
+    conv at hm₂ =>
       rw [ne_eq, Nat.factorization_eq_zero_iff]
       simp
-    have bla' : l % 2 = 0 := bla'.right.left
+    have hm₂ : l % 2 = 0 := hm₂.right.left
     match m₅ with
     | 0 =>
-      have bla : l % 10 ≠ 0 := by
+      have l_mod_ten_ne_zero : l % 10 ≠ 0 := by
         conv at hm₅ =>
-          unfold destruct
           rw [Nat.factorization_eq_zero_iff, eq_true Nat.prime_five, eq_false hl]
           simp
         rw [ne_eq, ← Nat.dvd_iff_mod_eq_zero, show 10 = 2 * 5 by rfl]
         by_contra ha
         absurd dvd_of_mul_left_dvd ha
         exact hm₅
-      rw [Nat.digits_def' (by simp) (Nat.zero_lt_of_ne_zero hl), List.find?_cons, decide_eq_true bla]
+      rw [Nat.digits_def' (by simp) (Nat.zero_lt_of_ne_zero hl), List.find?_cons]
+      rw [decide_eq_true l_mod_ten_ne_zero]
       simp
-      exact bla'
+      exact hm₂
     | m₅ + 1 =>
-      have bla : ¬l % 10 ≠ 0 := by
+      have l_mod_ten_eq_zero : ¬l % 10 ≠ 0 := by
         rw [ne_eq, not_not, ← Nat.dvd_iff_mod_eq_zero, show 10 = 2 * 5 by rfl]
         apply Nat.Prime.dvd_mul_of_dvd_ne (by simp) Nat.prime_two Nat.prime_five
         · rw [Nat.dvd_iff_mod_eq_zero]
-          exact bla'
+          exact hm₂
         · have hm₅' : l.factorization 5 ≠ 0 := by
             rw [hm₅]
             simp
           conv at hm₅' =>
-            unfold destruct
             rw [ne_eq, Nat.factorization_eq_zero_iff]
             simp
           exact hm₅'.right.left
-      rw [Nat.digits_def' (by simp) (Nat.zero_lt_of_ne_zero hl), List.find?_cons, decide_eq_false bla]
+      rw [Nat.digits_def' (by simp) (Nat.zero_lt_of_ne_zero hl), List.find?_cons]
+      rw [decide_eq_false l_mod_ten_eq_zero]
       simp only
-      rw [ne_eq, not_not, ← Nat.dvd_iff_mod_eq_zero] at bla
+      rw [ne_eq, not_not, ← Nat.dvd_iff_mod_eq_zero] at l_mod_ten_eq_zero
       have l_div_ten_ne_zero : l / 10 ≠ 0 := by
-        rw [Nat.div_ne_zero_iff_of_dvd bla]
+        rw [Nat.div_ne_zero_iff_of_dvd l_mod_ten_eq_zero]
         simp
         exact hl
       have m₂_add_one_eq_m₂ : (l / 10).factorization 2 + 1 = l.factorization 2 := by
         rw [show 1 = Nat.factorization 10 2 by native_decide]
-        rw (config := { occs := .pos [2] }) [← show l / 10 * 10 = l from Nat.div_mul_cancel bla]
+        rw (config := { occs := .pos [2] }) [← show l / 10 * 10 = l from Nat.div_mul_cancel l_mod_ten_eq_zero]
         rw [Nat.factorization_mul l_div_ten_ne_zero (by simp)]
         simp
       have m₅_add_one_eq_m₅ : (l / 10).factorization 5 + 1 = l.factorization 5 := by
         rw [show 1 = Nat.factorization 10 5 by native_decide]
-        rw (config := { occs := .pos [2] }) [← show l / 10 * 10 = l from Nat.div_mul_cancel bla]
+        rw (config := { occs := .pos [2] }) [← show l / 10 * 10 = l from Nat.div_mul_cancel l_mod_ten_eq_zero]
         rw [Nat.factorization_mul l_div_ten_ne_zero (by simp)]
         simp
       refine aux m₅ l_div_ten_ne_zero ?_ ?_
-      · rw [← Nat.add_one_lt_add_one_iff]
-        rw [m₂_add_one_eq_m₂, m₅_add_one_eq_m₅]
+      · rw [← Nat.add_one_lt_add_one_iff, m₂_add_one_eq_m₂, m₅_add_one_eq_m₅]
         exact hm₅m₂
       · rw [← Nat.add_one_inj, ← hm₅]
         exact m₅_add_one_eq_m₅
